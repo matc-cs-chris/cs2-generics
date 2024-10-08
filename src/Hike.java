9e20b418b6c57c3e76e586b8bc65fb91f93a71fc
matc-cs-chris <cpeterson16@madisoncollege.edu>
2024-10-08 09:12:37 -0500

    initial commit

---
+++ src/Hike.java
@@ -0,0 +1,26 @@
+public class Hike<T extends Number> implements Comparable<Hike<T>> {
+    private T distance;
+    private T maxElevation;
+
+    public Hike(T distance, T maxElevation) {
+        this.distance = distance;
+        this.maxElevation = maxElevation;
+    }
+
+    public static <S extends Number> double getHikeDifficulty(Hike<S> hike) {
+        return hike.distance.doubleValue() * hike.maxElevation.doubleValue();
+    }
+
+    @Override
+    public int compareTo(Hike<T> other) {
+        //TODO for equal case need to compare doubles to certain precision
+        double thisDifficulty = Hike.getHikeDifficulty(this);
+        double otherDifficulty = Hike.getHikeDifficulty(other);
+
+        return (int) (thisDifficulty - otherDifficulty);
+    }
+
+    public static void main(String[] args) {
+        //compare some hikes
+    }
+}
